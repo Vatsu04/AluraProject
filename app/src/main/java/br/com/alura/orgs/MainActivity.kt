e1384c4564e91126d00bda31f56e02874c24e6fa
Gustavo Passos Tascheri <98766671+Vatsu04@users.noreply.github.com>
2025-01-23 21:30:47 -0300

    Implementando o layout do Android

--- app/src/main/java/br/com/alura/orgs/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs/MainActivity.kt
@@ -3,13 +3,16 @@ package br.com.alura.orgs
 import android.app.Activity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 
 
 class MainActivity : Activity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val view = TextView(this)
+        view.setText("cesta de frutas")
+        setContentView(view)
     }
 
 
